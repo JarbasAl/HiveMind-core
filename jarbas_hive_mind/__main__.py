532a9777bcccc8ba994671e59837a0ef46f180e5
Joan <joanguitar@hotmail.com>
2021-09-15 10:52:12 -0400

    HiveMined-server database functionality split
    The database functionality is now accessed through HiveMind-database instead

--- jarbas_hive_mind/__main__.py
+++ jarbas_hive_mind/__main__.py
@@ -4,22 +4,14 @@ from jarbas_hive_mind.configuration import CONFIGURATION
 def main():
     import argparse
     parser = argparse.ArgumentParser(description='Start HiveMind as a server')
-    parser.add_argument("--name", help="human readable name")
-    parser.add_argument("--access_key", help="access key")
-    parser.add_argument("--crypto_key", help="payload encryption key")
-    parser.add_argument("--port", help="HiveMind port number")
+    parser.add_argument("--port", help="HiveMind port number", type=int)
     args = parser.parse_args()
-    # Check if a user was defined
-    if args.name is not None:
-        from jarbas_hive_mind.database import ClientDatabase
-        with ClientDatabase() as db:
-            db.add_client(args.name, args.access_key, crypto_key=args.crypto_key)
     config = CONFIGURATION
     listener = get_listener()
     listener.load_config(config)
     # Replace defined values
     if args.port is not None:
-        listener.port = int(args.port)
+        listener.port = args.port
     listener.listen()
 
 if __name__ == '__main__':
