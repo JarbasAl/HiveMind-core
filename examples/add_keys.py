d1952151992eb3a647d889bc01ef90d6884a4568
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-06 19:45:16 +0800

    Delete examples directory

--- examples/add_keys.py
+++
@@ -1,9 +0,0 @@
-from jarbas_hive_mind.database import ClientDatabase
-
-name = "JarbasCliTerminal"
-key = "RESISTENCEisFUTILE"
-crypto_key = "resistanceISfutile"
-
-
-with ClientDatabase() as db:
-    db.add_client(name, key, crypto_key=crypto_key)
